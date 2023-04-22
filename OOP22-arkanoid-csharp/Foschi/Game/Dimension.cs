5296b9d34d8ee0672c483538ef8f069ed7f5f9da
Virginia Foschi <virginia.foschi@studio.unibo.it>
2023-04-22 15:22:15 +0200

    create class Dimension

---
+++ OOP22-arkanoid-csharp/Foschi/Game/Dimension.cs
@@ -0,0 +1,32 @@
+namespace Foschi.Game {
+
+    public class Dimension {
+
+        private double _height;
+        private double _width;
+
+        public Dimension(double height, double width) 
+        {
+            _height=height;
+            _width=width;
+        }
+
+        public double Height
+        {
+            get {return _height;}
+            set {_height=value;}
+        }
+
+        public double Width
+        {
+            get {return _width;}
+            set {_width=value;}
+        }
+
+        public void IncreaseWidth(double value)
+        {
+            _width=_width+value;
+        }
+
+    }
+}
