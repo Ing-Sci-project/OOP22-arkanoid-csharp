8c7e26558970618fdd0247dd002e1edee5657f9a
Virginia Foschi <virginia.foschi@studio.unibo.it>
2023-04-22 17:07:06 +0200

    add comments in class Obstacle

--- OOP22-arkanoid-csharp/Foschi/Game/Obstacle.cs
+++ OOP22-arkanoid-csharp/Foschi/Game/Obstacle.cs
@@ -2,18 +2,33 @@ using Castiglioni.Api;
 using Castiglioni.Game;
 
 namespace Foschi.Game {
+
+    /// <summary>
+    /// class of obstacles that are objects that can't be destroyed.
+    /// </summary>
     public class Obstacle : AbstractBrick
     {
-        public Obstacle(BrickType type, Dimension d, Tuple<double, double> pos) : base(type, d, pos){}
+        /// <summary>
+        /// costructor of this class.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="d"></param>
+        /// <param name="pos"></param>
+        public Obstacle(BrickType type, Dimension d, Tuple<double, double> pos) : base(type, d, pos) {}
 
+        /// <inheritdoc />
         public override void DecreaseRes(int res) {}
 
+        /// <inheritdoc />
         public override int? GetRes() => null;
 
+        /// <inheritdoc />
         public override void Hit() {}
 
+        /// <inheritdoc />
         public override void IncreaseRes(int res) {}
 
+        /// <inheritdoc />
         public override bool IsDestroyed() => false;
     }
 }
