70c303f11a80176144d64a887391b1d160c022c9
Virginia Foschi <virginia.foschi@studio.unibo.it>
2023-04-22 08:49:52 +0200

    create inner class User and implement methods to access file

---
+++ OOP22-arkanoid-csharp/Foschi/Game/LeaderBoard.cs
@@ -0,0 +1,74 @@
+using Foschi.Api;
+using Newtonsoft.Json;
+
+namespace Foschi.Game 
+{
+    public class LeaderBoard : ILeaderBoard
+    {
+        private string _path;
+        private readonly int MAX=5;
+
+        public LeaderBoard() {
+            _path=Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),"aRkAnOiD_csharp.txt");
+            var f = new FileInfo(_path);
+            if (!File.Exists(_path) || f.Length == 0) {
+                FileStream str = File.Create(_path);
+                str.Close();
+            }
+        }
+        public List<Tuple<string, int>> GetBestFive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int? GetPoints(string name, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePoints(string name, string password, int points, int levelId)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Writeonfile(List<User> list) 
+        {
+            File.WriteAllText(_path, JsonConvert.SerializeObject(list));
+        }
+
+        private List<User> PlayersFromFile() {
+            List<User>? list = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText(_path));
+            return list==null? new List<User>() : list;
+        }
+
+        private List<User> LoadFromResources() 
+        {
+            List<User>? list = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("Resources/File.txt"));
+            return list==null? new List<User>() : list;
+        }
+
+        [Serializable]
+        class User {
+            public string _name {get;}
+            public string _password {get;}
+
+            public IDictionary<string,int> _points;
+
+            public User(string name, string password)
+            {
+                _name=name;
+                _password=password;
+                _points=new Dictionary<string,int>();
+            }
+
+            public int GetSumPoints() => _points.Sum(x=>x.Value);
+
+            public void Update(int points, int levelId)
+            {
+                _points[levelId.ToString()]=points;
+            }
+
+            public override string ToString() => _name+" "+GetSumPoints();
+        }
+    }
+}
