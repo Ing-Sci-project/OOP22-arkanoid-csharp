e2d74cf62e96511c9d59629c9c4578fdbc89566d
Virginia Foschi <virginia.foschi@studio.unibo.it>
2023-04-22 09:31:10 +0200

    create leaderBoard test

--- OOP22-arkanoid-csharp/Foschi/Test/testClass.cs
+++ OOP22-arkanoid-csharp/Foschi/Test/testClass.cs
@@ -10,6 +10,7 @@ namespace Foschi.Test{
         private readonly double Y = 4.0;
         private readonly double INC_X = 0.43;
         private readonly double INC_Y = 0.43;
+        private  Random random = new Random();
 
         [Fact]
         public void testSpeed(){
@@ -21,6 +22,42 @@ namespace Foschi.Test{
             }
             speed2.sum(new Speed(20*INC_X,20*INC_Y));
             Assert.Equal(speed,speed2);
+        }
+
+        [Fact]
+        public void testLeaderBoard(){
+            ILeaderBoard l = new LeaderBoard();
+            string name=RandomString();
+            string password=RandomString();
+            bool trovato = true;
+            while(trovato) {
+                if(!l.GetPoints(name,password).HasValue) {
+                    trovato=false;
+                } else {
+                    name=RandomString();
+                    password=RandomString();
+                }
+            }
+            Assert.True(!l.GetPoints(name,password).HasValue);
+            l.UpdatePoints(name,password,30,1);
+            Assert.True(l.GetPoints(name,password).HasValue && l.GetPoints(name,password) == 30);
+            l.UpdatePoints(name,password,10,2);
+            Assert.True(l.GetPoints(name,password) == 40);
+            l.UpdatePoints(name,password,10,1);
+            Assert.True(l.GetPoints(name,password) == 20);
+            l.UpdatePoints(name,password,80,3);
+            Assert.True(l.GetPoints(name,password) == 100);
+            l.UpdatePoints(name,password,60,2);
+            Assert.True(l.GetPoints(name,password) == 150);
+            l.UpdatePoints(name,password,20,1);
+            Assert.True(l.GetPoints(name,password) == 160);
+        }
+
+        private string RandomString()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, 5)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
