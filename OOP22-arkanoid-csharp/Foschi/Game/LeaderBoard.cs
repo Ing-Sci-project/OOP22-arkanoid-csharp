2932569f9d1495fbbc5591582e214574bd9a2514
Virginia Foschi <virginia.foschi@studio.unibo.it>
2023-04-22 17:25:42 +0200

    add comments in class and interface LeaderBoard

--- OOP22-arkanoid-csharp/Foschi/Game/LeaderBoard.cs
+++ OOP22-arkanoid-csharp/Foschi/Game/LeaderBoard.cs
@@ -3,11 +3,17 @@ using Newtonsoft.Json;
 
 namespace Foschi.Game 
 {
+    /// <summary>
+    /// Class that manages ranking of the game.
+    /// </summary>
     public class LeaderBoard : ILeaderBoard
     {
         private string _path;
         private readonly int MAX=5;
 
+        /// <summary>
+        /// costructor of this class.
+        /// </summary>
         public LeaderBoard() {
             _path=Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),"aRkAnOiD_csharp.txt");
             var f = new FileInfo(_path);
@@ -17,6 +23,8 @@ namespace Foschi.Game
                 WriteOnFile(LoadFromResources());
             }
         }
+
+        /// <inheritdoc />
         public List<Tuple<string, int>> GetBestFive()
         {
             List<User> list = PlayersFromFile();
@@ -26,12 +34,14 @@ namespace Foschi.Game
             return list.Select(x=>new Tuple<string,int>(x._name,x.GetSumPoints())).ToList();
         }
 
+        /// <inheritdoc />
         public int? GetPoints(string name, string password)
         {
             User? usr = PlayersFromFile().Find(x=>x._name.Equals(name) && x._password.Equals(password));
             return usr == null ? null : usr.GetSumPoints();
         }
 
+        /// <inheritdoc />
         public void UpdatePoints(string name, string password, int points, int levelId)
         {
             List<User> list = PlayersFromFile();
@@ -47,24 +57,43 @@ namespace Foschi.Game
             WriteOnFile(list);
         }
 
+        /// <summary>
+        /// method to check if a player exists.
+        /// </summary>
+        /// <param name="usr"> name of the player</param>
+        /// <param name="password"> password of the player</param>
+        /// <param name="list"> list of players</param>
+        /// <returns>null if not exists</returns>
         private User? CheckUser(string usr, string password, List<User> list) => list.Find(x=>x._name.Equals(usr) && x._password.Equals(password));
 
-        private void WriteOnFile(List<User> list) 
-        {
-            File.WriteAllText(_path, JsonConvert.SerializeObject(list));
-        }
+        /// <summary>
+        /// method to write leaderboard on file.
+        /// </summary>
+        /// <param name="list"> list of players</param>
+        private void WriteOnFile(List<User> list) => File.WriteAllText(_path, JsonConvert.SerializeObject(list));
 
+        /// <summary>
+        /// method to get list of players from file.
+        /// </summary>
+        /// <returns>list of players</returns>
         private List<User> PlayersFromFile() {
             List<User>? list = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText(_path));
             return list==null? new List<User>() : list;
         }
 
+        /// <summary>
+        /// method to get list of players from resources file.
+        /// </summary>
+        /// <returns>list of players</returns>
         private List<User> LoadFromResources() 
         {
             List<User>? list = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("Resources/File.txt"));
             return list==null? new List<User>() : list;
         }
 
+        /// <summary>
+        /// Class that manages informations of each palyer.
+        /// </summary>
         [Serializable]
         public class User {
             public string _name {get; set;}
@@ -72,6 +101,11 @@ namespace Foschi.Game
 
             public Dictionary<string,int> _points;
 
+            /// <summary>
+            /// Constructor of this class.
+            /// </summary>
+            /// <param name="name"> name of the player</param>
+            /// <param name="password"> password of the player</param>
             public User(string name, string password)
             {
                 _name=name;
@@ -79,13 +113,23 @@ namespace Foschi.Game
                 _points=new Dictionary<string,int>();
             }
 
+            /// <summary>
+            /// method to get player's points.
+            /// </summary>
+            /// <returns>player's points</returns>
             public int GetSumPoints() => _points.Sum(x=>x.Value);
 
+            /// <summary>
+            /// method to update player's points.
+            /// </summary>
+            /// <param name="points"> points totalize by the player</param>
+            /// <param name="levelId"> level done</param>
             public void Update(int points, int levelId)
             {
                 _points[levelId.ToString()]=points;
             }
 
+            /// <inheritdoc />
             public override string ToString() => _name+" "+GetSumPoints();
         }
     }
