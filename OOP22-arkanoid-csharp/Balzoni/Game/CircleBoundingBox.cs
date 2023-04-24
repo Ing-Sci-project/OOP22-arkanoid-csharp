eb789377cd6f66c4574b4e95bbb539776d6693e0
Margherita <margherita.balzoni@studio.unibo.it>
2023-04-24 17:05:07 +0200

    Fixed CircleBoundingBox class

--- OOP22-arkanoid-csharp/Balzoni/Game/CircleBoundingBox.cs
+++ OOP22-arkanoid-csharp/Balzoni/Game/CircleBoundingBox.cs
@@ -1,9 +1,10 @@
 using Balzoni.Api;
+using Shapes;
 namespace  Balzoni.Game
 {
     public class CircleBoundingBox : AbstractBoundingBox
     {
-        public CircleBoundingBox(IMovingObject obj) : base(obj.getDimension().getWidth(), obj.getDimension().getHeight(), obj.getPos())
+        public CircleBoundingBox(IMovingObject obj) : base(obj.Dimension.Width, obj.Dimension.Height, obj.Pos)
         {
 
         }
