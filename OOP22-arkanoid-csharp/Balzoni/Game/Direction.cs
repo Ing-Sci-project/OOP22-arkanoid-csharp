8018e751b08b5c9c1ce7c8bc254e30d2584b5593
Margherita <margherita.balzoni@studio.unibo.it>
2023-04-24 16:58:32 +0200

    Fixed Direction class

--- OOP22-arkanoid-csharp/Balzoni/Game/Direction.cs
+++ OOP22-arkanoid-csharp/Balzoni/Game/Direction.cs
@@ -12,44 +12,45 @@ namespace  Balzoni.Game
         public Direction(){
              _d = new Tuple<int, int>(Left, Up);
         }
+         /// <inheritdoc />
         public Tuple<int, int> getDirection() => _d;
-       
+        /// <inheritdoc />
         public bool isDirectionLeft() => (_d.Item1 == Left);
-
+        /// <inheritdoc />
         public bool isDirectionRight() => (_d.Item1 == Right);
-
+         /// <inheritdoc />
         public bool isDirectionUp() => (_d.Item2 == Up);
-
+         /// <inheritdoc />
         public void resetDirection()
         {
              _d = new Tuple<int, int>(Left, Up);
         }
-
+        /// <inheritdoc />
         public void setCentre()
         {
              _d = new Tuple<int, int>(0, Up);
         }
-
+         /// <inheritdoc />
         public void setDirection(Tuple<int, int> new_D)
         {
             _d= new_D;
         }
-
+         /// <inheritdoc />
         public void setDirectionDown()
         {
              _d = new Tuple<int, int>(_d.Item1, Down);
         }
-
+         /// <inheritdoc />
         public void setDirectionLeft()
         {
              _d = new Tuple<int, int>(Left, _d.Item2);
         }
-
+         /// <inheritdoc />
         public void setDirectionRight()
         {
              _d = new Tuple<int, int>(Right, _d.Item2);
         }
-
+         /// <inheritdoc />
         public void setDirectionUp()
         {
              _d = new Tuple<int, int>(_d.Item1, Up);
