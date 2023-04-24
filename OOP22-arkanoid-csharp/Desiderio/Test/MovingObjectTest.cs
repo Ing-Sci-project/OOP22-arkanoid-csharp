875551f41731f248039a3a8f8158e6dde071af70
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2023-04-24 16:29:32 +0200

    create test

--- OOP22-arkanoid-csharp/Desiderio/Test/MovingObjectTest.cs
+++ OOP22-arkanoid-csharp/Desiderio/Test/MovingObjectTest.cs
@@ -1,23 +1,25 @@
 // importare le librerie necessarie
 using Xunit;
 using Moq;
+using Castiglioni.Game;
+using MovingShapesImpl;
+using OOP22_arkanoid_csharp.Desiderio.Api;
+
+// importare le librerie necessarie
+using Xunit;
+using Foschi.Game;
 
 public class MovingObjectTest
 {
     [Fact]
     public void Ball_PositionIsSetCorrectly()
     {
-        // creare una dimensione di prova
-        var dimension = new Mock<IDimension>();
-        dimension.SetupGet(d => d.Width).Returns(10);
-        dimension.SetupGet(d => d.Height).Returns(10);
-
         // creare una palla di prova
-        var ball = new Ball(dimension.Object);
+        var ball = new Ball(new Dimension(10, 10));
 
         // verificare che la posizione sia stata impostata correttamente
-        var expectedX = SizeCalculation.GetWorldSize().Item2 / 2 - dimension.Object.Width / 2;
-        var expectedY = SizeCalculation.GetWorldSize().Item1 - 100 - 2 * dimension.Object.Height - 5;
+        var expectedX = SizeCalculation.GetWorldSize().Item2 / 2 - 5;
+        var expectedY = SizeCalculation.GetWorldSize().Item1 - 100 - 20 - 5;
         Assert.Equal(expectedX, ball.Pos.Item1);
         Assert.Equal(expectedY, ball.Pos.Item2);
     }
@@ -25,16 +27,11 @@ public class MovingObjectTest
     [Fact]
     public void Pad_PositionIsSetCorrectly()
     {
-        // creare una dimensione di prova
-        var dimension = new Mock<IDimension>();
-        dimension.SetupGet(d => d.Width).Returns(50);
-        dimension.SetupGet(d => d.Height).Returns(10);
-
         // creare una pedana di prova
-        var pad = new Pad(dimension.Object);
+        var pad = new Pad(new Dimension(50, 10));
 
         // verificare che la posizione sia stata impostata correttamente
-        var expectedX = SizeCalculation.GetWorldSize().Item2 / 2 - dimension.Object.Width / 2;
+        var expectedX = SizeCalculation.GetWorldSize().Item2 / 2 - 25;
         var expectedY = SizeCalculation.GetWorldSize().Item1 - 100;
         Assert.Equal(expectedX, pad.Pos.Item1);
         Assert.Equal(expectedY, pad.Pos.Item2);
